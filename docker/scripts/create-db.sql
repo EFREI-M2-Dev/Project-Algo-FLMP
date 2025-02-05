cf7a1c5c6a040ad0ed5d611692d58f1096312ffd
Flamby <66632621+Kaeggame@users.noreply.github.com>
2025-02-05 01:31:16 +0100

    Sql create table & use default entrypoint docker
    Sql file updated with the create table with id, text, positive, negative

--- docker/scripts/create-db.sql
+++ docker/scripts/create-db.sql
@@ -1,3 +1,10 @@
-CREATE DATABASE IF NOT EXISTS 'docker-db';
+CREATE DATABASE IF NOT EXISTS `mydatabase`;
 
-USE 'docker-db';
+USE `mydatabase`;
+
+CREATE TABLE IF NOT EXISTS `tweets` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `text` TEXT NOT NULL,
+    `positive` INT(1) NOT NULL DEFAULT 0,
+    `negative` INT(1) NOT NULL DEFAULT 0
+);
